62204442a780b1a305d6f234b8e59f53768a0a7b
longlam24 <longlam636@gmail.com>
2021-10-11 05:38:32 +0000

    update03

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -T -i ec2-user@18.162.44.113 /home/ec2-user/key.pem << EOF
+ssh -T -i ec2-user@18.162.41.107 /home/ec2-user/key.pem << EOF
 cd /var/www/html/servedemo
 git pull
 EOF
